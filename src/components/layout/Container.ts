9f3fd015cb8418625851b844296ee48941a59e5c
rmxway <rmxway@mail.ru>
2024-06-18 21:28:09 +0300

    Project init

---
+++ src/components/layout/Container.ts
@@ -0,0 +1,59 @@
+import styled, { css } from 'styled-components';
+
+import { media } from '@/src/theme';
+
+/**
+ * Adaptive container block
+ * @param {boolean} $pt - padding-top: 40px
+ * @param {boolean} $pb - padding-top: 40px
+ * @param {boolean} $flex - display: flex; flex-direction: column
+ */
+
+interface ContainerProps {
+	$pt?: boolean;
+	$pb?: boolean;
+	$flex?: boolean;
+}
+
+export const Container = styled.div<ContainerProps>`
+	position: relative;
+	display: block;
+	padding: 0 20px;
+	width: 100%;
+	flex-grow: 1;
+	max-width: 100%;
+	margin: 0 auto;
+
+	${({ $pt }) =>
+		$pt &&
+		css`
+			padding-top: 40px;
+		`}
+
+	${({ $pb }) =>
+		$pb &&
+		css`
+			padding-bottom: 40px;
+		`}
+
+	${({ $flex }) =>
+		$flex &&
+		css`
+			display: flex;
+			flex-direction: column;
+		`}
+
+	${media.greaterThan('sm')`
+		max-width: 100%;
+	`}
+
+	${media.greaterThan('md')`
+		max-width: 1024px;
+	`}
+
+	${media.greaterThan('lg')`
+		max-width: 1280px;
+	`}
+`;
+
+export default Container;
---
+++ src/theme/defaultTheme.ts
@@ -0,0 +1,52 @@
+import { darken, desaturate } from 'polished';
+import { DefaultTheme } from 'styled-components';
+
+import { Colors, Gradients } from '@/@types/styled';
+
+const primary = '#007536';
+const success = '#4E9600';
+const danger = '#A62500';
+const label = '#034569';
+
+const colors: Colors = {
+	primary,
+	success,
+	danger,
+	label,
+	dark: desaturate(0.9, darken(0.2, success)),
+	gray: {
+		$1: '#f3f3f3',
+		$2: '#e3e3e3',
+		$3: '#cdcdcd',
+		$4: '#bebebe',
+		$5: '#a5a5a5',
+		$6: '#747474',
+		$7: '#595959',
+		$8: '#434343',
+		$9: '#222222',
+	},
+};
+
+export const createGradient = (deg: string, color1: string, long1: string, color2: string, long2: string) =>
+	`linear-gradient(${deg}, ${color1} ${long1}, ${color2} ${long2})`;
+
+const gradients: Gradients = {
+	main: (deg = '150deg') => createGradient(deg, '#e8ffb3', '0%', success, '60%'),
+	dark: (deg = '150deg') => createGradient(deg, success, '0%', label, '60%'),
+	softDark: (deg = '150deg') => createGradient(deg, success, '0%', '#fff', '40%'),
+};
+
+export const defaultTheme: DefaultTheme = {
+	name: 'default',
+	colors,
+	gradients,
+	layout: {
+		containerWidth: '1024px',
+		shadow: '0 5px 20px rgba(0, 0, 0, 0.2)',
+	},
+	radius: {
+		borderRadius: '8px',
+	},
+};
+
+export default defaultTheme;
